234e89758a5006f3f6f46620d6e4cdbf9cc9b0cf
Michael Plested <mike.gopolis@gmail.com>
2024-09-27 08:39:52 +0000

    Save progress: 1120.1

--- fortune.sh
+++ fortune.sh
@@ -2,4 +2,7 @@
 
 # Program to tell a persons fortune
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+
 echo -e "\n~~ Fortune Teller ~~\n"
+echo ${RESPONSES[5]}
+
