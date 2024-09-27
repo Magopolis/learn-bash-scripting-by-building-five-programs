8014deb92785598850d1d5f521ae8fa6ec98c4cc
Michael Plested <mike.gopolis@gmail.com>
2024-09-27 10:55:36 +0000

    Save progress: 1190.1

--- fortune.sh
+++ fortune.sh
@@ -7,4 +7,6 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 N=$(( RANDOM % 6 ))
 echo ${RESPONSES[$N]}
-GET_FORTUNE() {}
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+}
